7ae94a901a451300a8ad651970b2b3e59ff1556a
Felix.Khodakovsky <felix.khodakovsky@quantumsoft.ru>
2023-09-29 07:54:49 +1000

    add proxy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/cats',
+        destination: 'https://meowfacts.herokuapp.com',
+      },
+      {
+        source: '/face',
+        destination: 'https://facebook.com',
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
